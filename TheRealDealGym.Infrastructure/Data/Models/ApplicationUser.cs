e03597c214720e2efd3b5a9fffb793dcb32529c7
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-02 17:47:47 +0300

    User extended with FirstName and LastName. Login and Registered pages scaffolded and configured with the above-mentioned custom user extensions - ApplicationUser

--- TheRealDealGym.Infrastructure/Data/Models/ApplicationUser.cs
+++ TheRealDealGym.Infrastructure/Data/Models/ApplicationUser.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace TheRealDealGym.Infrastructure.Data.Models
 {
     public class ApplicationUser : IdentityUser<Guid>
     {
+        public ApplicationUser()
+        {
+                this.Id = Guid.NewGuid();
+        }
 
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
     }
 }
