0b78c9ce0bc220403120d85aa502fb5757467f06
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-02 18:39:48 +0300

    Room entity class created

---
+++ TheRealDealGym.Infrastructure/Constants/ValidationConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace TheRealDealGym.Infrastructure.Constants
+{
+    public static class ValidationConstants
+    {
+        public static class ForRoom
+        {
+            public const int NameMinLength = 2;
+            public const int NameMaxLength = 40;
+            public const int MinCapacity = 10;
+            public const int MaxCapacity = 40;
+        }
+    }
+}
---
+++ TheRealDealGym.Infrastructure/Data/Models/Room.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static TheRealDealGym.Infrastructure.Constants.ValidationConstants.ForRoom;
+namespace TheRealDealGym.Infrastructure.Data.Models
+{
+    /// <summary>
+    /// The room is the place where a specific type of classes will be taking place.
+    /// </summary>
+    [Comment("The room is the place where a specific type of classes will be taking place.")]
+    public class Room
+    {
+        public Room()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Room identifier.
+        /// </summary>
+        [Key]
+        [Comment("Room identifier")]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The name of the room. Helps the customers find where the class they've booked for will take place.
+        /// Also helps with search filtering.
+        /// </summary>
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Comment("The name of the room")]
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// The maximum capacity of the room. This shows how many people can book for a specific class.
+        /// Helps keeping track of the bookings and available spaces.
+        /// </summary>
+        [Required]
+        [Range(MinCapacity, MaxCapacity)]
+        [Comment("The maximum capacity of the room")]
+        public int Capacity { get; set; }
+
+        /// <summary>
+        /// Serves a soft delete purpose.
+        /// </summary>
+        [Comment("Serves a soft delete purpose")]
+        public bool IsDeleted { get; set; } = false;
+    }
+}
