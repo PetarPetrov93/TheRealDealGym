60a06b7c6d7a745315d760396bbabcfde7f791a4
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-03 10:39:54 +0300

    DbSets implemented. Query filter implemented for the db sets in regards to the soft delete ot ignore softly-deleted entities by default.

--- TheRealDealGym.Infrastructure/Data/ApplicationDbContext.cs
+++ TheRealDealGym.Infrastructure/Data/ApplicationDbContext.cs
@@ -14,12 +14,48 @@ namespace TheRealDealGym.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<Room>(entity =>
+            {
+                entity.HasQueryFilter(r => !r.IsDeleted);
+            });
+
+            builder.Entity<Sport>(entity =>
+            {
+                entity.HasQueryFilter(s => !s.IsDeleted);
+            });
+
+            builder.Entity<Trainer>(entity =>
+            {
+                entity.HasQueryFilter(t => !t.IsDeleted);
+            });
+
             builder.Entity<TrainerSport>(entity =>
             {
                 entity.HasKey(ts => new { ts.TrainerId, ts.SportId });
+                entity.HasQueryFilter(ts => !ts.Trainer.IsDeleted && !ts.Sport.IsDeleted);
+            });
+
+            builder.Entity<Booking>(entity =>
+            {
+                entity.HasQueryFilter(b => !b.IsDeleted);
+            });
+
+            builder.Entity<Class>(entity =>
+            {
+                entity.HasQueryFilter(c => !c.IsDeleted);
             });
 
             base.OnModelCreating(builder);
         }
+
+        /// <summary>
+        /// These DbSets represent each table in the database.
+        /// </summary>
+        public DbSet<Room> Rooms { get; set; } = null!;
+        public DbSet<Sport> Sports { get; set; } = null!;
+        public DbSet<Trainer> Trainers { get; set; } = null!;
+        public DbSet<TrainerSport> TrainerSports { get; set; } = null!;
+        public DbSet<Booking> Bookings { get; set; } = null!;
+        public DbSet<Class> Classes { get; set; } = null!;
     }
 }
