70179086dcdbf15a1bab8e683ea00a35fd078952
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-09 13:51:33 +0300

    Some basic methods added to the IRepository interface. Not currently implemented in the Repository class!

--- TheRealDealGym.Infrastructure/Data/Common/Repository.cs
+++ TheRealDealGym.Infrastructure/Data/Common/Repository.cs
@@ -1,6 +1,43 @@
-﻿namespace TheRealDealGym.Infrastructure.Data.Common
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace TheRealDealGym.Infrastructure.Data.Common
 {
     public class Repository : IRepository
     {
+        private readonly DbContext context;
+        public Repository(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+        public Task AddAsync<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync<T>(object id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
