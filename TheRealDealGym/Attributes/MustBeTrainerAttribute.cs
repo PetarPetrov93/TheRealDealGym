4813f7462dbeb6b21c7b692337c8335dad7844c5
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-09 16:51:15 +0300

    Must be trainer attribute created. Not yet implemented.

---
+++ TheRealDealGym/Attributes/MustBeTrainerAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TheRealDealGym.Attributes
+{
+    // TODO: IMPLEMENT THIS ATTRIBUTE WHEN TRAINERSERVICE IS CREATED
+    /// <summary>
+    /// This is a custom attribute which checks if the logged-in user is a trainer.
+    /// This attribute constraints only users who are trainers to be able to access a specific action in a controller. 
+    /// </summary>
+    public class MustBeTrainerAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            //ITrainerService? trainerService = context.HttpContext.RequestServices.GetService<ITrainerService>();
+
+            //if (trainerService == null)
+            //{
+            //    context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            //}
+
+            //if (trainerService != null && trainerService.ExistsByIdAsync(context.HttpContext.User.GetId()).Result == false)
+            //{
+            //    context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            //}
+        }
+    }
+}
