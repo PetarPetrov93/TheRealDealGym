0358f0b588a961588818a7434258f7e9ad4973c8
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-09 16:33:24 +0300

    custom decimal model binder and model binder provider implemented.

--- TheRealDealGym/Attributes/IsNotATrainerAttribute.cs
+++ TheRealDealGym/Attributes/IsNotATrainerAttribute.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
-using System.Security.Claims;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace TheRealDealGym.Attributes
 {
