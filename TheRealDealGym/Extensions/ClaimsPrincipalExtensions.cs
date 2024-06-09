7158db26ba8651063d22665c6a3cd57004b2bc23
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-09 11:44:40 +0300

    GetId() implemented. This will help to get the current user Id easier wherever and whenever needed

---
+++ TheRealDealGym/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Security.Claims
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
