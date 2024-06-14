d0777929e8d639b23782f569ff5eec7affebebd3
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-14 19:32:23 +0300

    Trainer service implemented

---
+++ TheRealDealGym.Core/Contracts/ITrainerService.cs
@@ -0,0 +1,12 @@
+﻿using TheRealDealGym.Core.Models.Trainer;
+
+namespace TheRealDealGym.Core.Contracts
+{
+    public interface ITrainerService
+    {
+        Task<bool> ExistsByIdAsync(Guid userId);
+        Task CreateAsync(Guid userId, JobApplication trainerInfo);
+        Task<Guid?> GetTrainerIdAsync(Guid userId);
+        Task<TrainerProfileInfo> GetTrainerProfileInfoAsync(Guid trainerId);
+    }
+}
