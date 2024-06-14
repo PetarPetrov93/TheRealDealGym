d88e9561ae32c79cccb55cc740cdee5e7ebcff51
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-14 15:16:08 +0300

    Booking ViewModel created

---
+++ TheRealDealGym.Core/Models/Booking/BookingModel.cs
@@ -0,0 +1,17 @@
+﻿using TheRealDealGym.Core.Models.Class;
+
+namespace TheRealDealGym.Core.Models.Booking
+{
+    /// <summary>
+    /// This ViewModel displays a booking made by the loged-in user
+    /// </summary>
+    public class BookingModel
+    {
+        public Guid Id { get; set; }
+
+        public string Class { get; set; } = null!;
+
+        public ClassModel ClassModel { get; set; } = null!;
+
+    }
+}
