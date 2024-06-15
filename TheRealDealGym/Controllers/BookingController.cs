2bf8b0e02edfd261b831a3664b6bab860df503be
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-15 18:43:09 +0300

    BookingController actions created. Not implemented

--- TheRealDealGym/Controllers/BookingController.cs
+++ TheRealDealGym/Controllers/BookingController.cs
@@ -8,8 +8,37 @@ namespace TheRealDealGym.Controllers
     /// </summary>
     public class BookingController : BaseController
     {
+        /// <summary>
+        /// This action displays all the bookings the logged-in user has made. Accesible through the "My Bookings" button.
+        /// </summary>
         [AllowAnonymous]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// This action is responsible for booking for a specific class from the "Schedule" page.
+        /// </summary>
+        [AllowAnonymous]
+        public IActionResult Book()
+        {
+            //No view needed for this action.
+            return RedirectToAction("Index", "Class");
+        }
+
+        /// <summary>
+        /// This action is responsible for cancelling a booking for a specific class from the "Schedule" page.
+        /// </summary>
+        [AllowAnonymous]
+        public IActionResult CancelBooking()
+        {
+            //No view needed for this action.
+            return RedirectToAction("Index", "Class");
+        }
+
+        [AllowAnonymous]
+        public IActionResult Info()
         {
             return View();
         }
