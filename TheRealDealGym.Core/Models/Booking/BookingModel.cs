e377a85e0c84f585e489a085a569ae98192d5635
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-16 12:15:17 +0300

    Booking model decription changed

--- TheRealDealGym.Core/Models/Booking/BookingModel.cs
+++ TheRealDealGym.Core/Models/Booking/BookingModel.cs
@@ -3,14 +3,12 @@
 namespace TheRealDealGym.Core.Models.Booking
 {
     /// <summary>
-    /// This ViewModel displays a booking made by the loged-in user
+    /// This ViewModel displays a booking made by the loged-in user in his "My Bookings" page.
     /// </summary>
     public class BookingModel
     {
         public Guid Id { get; set; }
-
         public string Class { get; set; } = null!;
-
         public string Date { get; set; } = null!;
         public string Time { get; set; } = null!;
     }
