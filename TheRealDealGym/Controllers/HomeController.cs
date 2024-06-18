1e61ce03932a0caf071d8e690e9bfd04f56e7917
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-18 14:09:45 +0300

    Error action changed. Status code checks implemented

--- TheRealDealGym/Controllers/HomeController.cs
+++ TheRealDealGym/Controllers/HomeController.cs
@@ -33,9 +33,26 @@ namespace TheRealDealGym.Controllers
 
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+            if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+            if (statusCode == 500)
+            {
+                return View("Error500");
+            }
+
+            return View();
         }
     }
 }
