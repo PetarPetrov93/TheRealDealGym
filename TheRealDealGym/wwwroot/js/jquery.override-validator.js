c12b4a38bff20f2ff3ee7374a0882cac63ef3545
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-20 16:55:51 +0300

    JQuery override-validator fixed.

--- TheRealDealGym/wwwroot/js/jquery.override-validator.js
+++ TheRealDealGym/wwwroot/js/jquery.override-validator.js
@@ -1,9 +1,9 @@
-﻿jQuery(function ($) {
+﻿﻿jQuery(function ($) {
     $.validator.addMethod('number', function (value, element) {
         return this.optional(element) || /^-?(?:\d+)(?:(\.|,)\d+)?$/.test(value);
     });
 
-    $.validator.range = function (value, element, param) {
+    $.validator.methods.range = function (value, element, param) {
         return this.optional(element) || (Number(value.replace(',', '.')) >= Number(param[0]) && Number(value.replace(',', '.')) <= Number(param[1]));
     }
 });
