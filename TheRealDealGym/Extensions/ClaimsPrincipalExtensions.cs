cda1125f4bb610ee8a47cb9cba9f72d2ca4a8f6f
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-21 11:05:13 +0300

    Calims orincipal extedned with a method which checks if the logged-in user is admin.

--- TheRealDealGym/Extensions/ClaimsPrincipalExtensions.cs
+++ TheRealDealGym/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,4 +1,5 @@
-﻿namespace System.Security.Claims
+﻿using static TheRealDealGym.Core.Constants.RoleConstants;
+namespace System.Security.Claims
 {
     /// <summary>
     /// User claims principlal extentions.
@@ -11,6 +12,14 @@
         public static Guid GetId(this ClaimsPrincipal user)
         {
             return Guid.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier));
+        }
+
+        /// <summary>
+        /// This method checks if the logged-in user is Admin.
+        /// </summary>
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRole);
         }
     }
 }
