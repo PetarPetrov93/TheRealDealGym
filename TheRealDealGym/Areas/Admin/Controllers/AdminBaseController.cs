3c7644fcb4cc4310f24c286ed625af1acd3d0774
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-21 12:32:16 +0300

    Admin area and interface implemented.

---
+++ TheRealDealGym/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static TheRealDealGym.Core.Constants.RoleConstants;
+
+namespace TheRealDealGym.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = AdminRole)]
+    public class AdminBaseController : Controller
+    {
+    }
+}
