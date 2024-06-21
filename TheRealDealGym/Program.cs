ca95773878a40e3fecef8fdf6a9f1fd19a25caf2
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-21 12:52:10 +0300

    Link to admin interface from the public interface implemented

--- TheRealDealGym/Program.cs
+++ TheRealDealGym/Program.cs
@@ -39,12 +39,12 @@ app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-    endpoints.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
     endpoints.MapRazorPages();
 });
