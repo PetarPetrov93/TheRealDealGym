8c2542364da1bb3f7be78f2a259aa37228dd9d79
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-23 19:08:38 +0300

    Careers view with list of available jobs.

--- TheRealDealGym/Controllers/CareerController.cs
+++ TheRealDealGym/Controllers/CareerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TheRealDealGym.Core.Contracts;
 
 namespace TheRealDealGym.Controllers
 {
@@ -8,9 +9,45 @@ namespace TheRealDealGym.Controllers
     /// </summary>
     public class CareerController : BaseController
     {
+        private readonly IJobService jobService;
+
+        public CareerController(IJobService _jobService)
+        {
+            jobService = _jobService;
+        }
+
+
+        /// <summary>
+        /// This action gets all job adverts basic information.
+        /// </summary>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var model = await jobService.AllJobsAsync();
+
+            return View(model);
+        }
+
+        /// <summary>
+        /// This action displays the full information about the job advert upon pressing the "Details" button.
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid jobAdvertId)
+        {
+            if (await jobService.ExistsByIdAsync(jobAdvertId) == false)
+            {
+                return BadRequest();
+            }
+
+            var model = await jobService.GetByIdAsync(jobAdvertId);
+
+            return View(model);
+        }
+
         [AllowAnonymous] //Subject to change depending on whether a non-registered user should be able to apply or not.
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Info()
         {
             return View();
         }
