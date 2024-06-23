d6181f50bfaa37542b5c6f7c379801fe50cd86c1
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-23 14:13:15 +0300

    Add edit and delete methods for Room in the RoomController implemented.

--- TheRealDealGym/Areas/Admin/Controllers/RoomController.cs
+++ TheRealDealGym/Areas/Admin/Controllers/RoomController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TheRealDealGym.Attributes;
 using TheRealDealGym.Core.Contracts;
+using TheRealDealGym.Core.Models.Class;
+using TheRealDealGym.Core.Models.Room;
+using TheRealDealGym.Core.Services;
 
 namespace TheRealDealGym.Areas.Admin.Controllers
 {
@@ -25,6 +30,99 @@ namespace TheRealDealGym.Areas.Admin.Controllers
             var model = await roomService.AllRoomsAsync();
 
             return View(model);
+        }
+
+        /// <summary>
+        /// This method returns a form to fill in order to add a new room.
+        /// </summary>
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// This method handles the input infromation from the form and creates a new room.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Add(RoomServiceModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(model);
+            }
+
+            Guid newRoom = await roomService.CreateAsync(model);
+            return RedirectToAction(nameof(Index), "Room");
+        }
+
+        /// <summary>
+        /// This method returns a form, pre-filled with the information of the room the admin wants to edit.
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid roomId)
+        {
+            if (await roomService.ExistsByIdAsync(roomId) == false)
+            {
+                return BadRequest();
+            }
+
+            var model = await roomService.GetRoomByIdAsync(roomId);
+
+            return View(model);
+        }
+
+        /// <summary>
+        /// This method handles the editted information from the admin and saves the changes.
+        /// </summary>
+        [HttpPost]
+        [MustBeTrainer]
+        public async Task<IActionResult> Edit(Guid roomId, RoomServiceModel model)
+        {
+            if (await roomService.ExistsByIdAsync(roomId) == false)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid == false)
+            {
+               
+                return View(model);
+            }
+
+            await roomService.EditAsync(roomId, model);
+
+            return RedirectToAction(nameof(Index), "Room");
+        }
+
+        ///// <summary>
+        ///// This method gets the information about the room that has to be deleted.
+        ///// </summary>
+        //[HttpGet]
+        //public async Task<IActionResult> Delete(Guid roomId)
+        //{
+        //    if (await roomService.ExistsByIdAsync(roomId) == false)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    return RedirectToAction(nameof(Index), "Room");
+        //}
+
+        /// <summary>
+        /// This method deteles the given room.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid roomId)
+        {
+            if (await roomService.ExistsByIdAsync(roomId) == false)
+            {
+                return BadRequest();
+            }
+
+            await roomService.DeleteAsync(roomId);
+
+            return RedirectToAction(nameof(Index), "Room");
         }
     }
 }
