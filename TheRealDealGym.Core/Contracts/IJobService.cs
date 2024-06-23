468632ffc11d1f9372d93828053d910c893ed3ae
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-06-23 15:17:44 +0300

    JobService add edit details activate and deactivate methods implemented.

--- TheRealDealGym.Core/Contracts/IJobService.cs
+++ TheRealDealGym.Core/Contracts/IJobService.cs
@@ -5,5 +5,11 @@ namespace TheRealDealGym.Core.Contracts
     public interface IJobService
     {
         Task<IEnumerable<JobAdvertListModel>> AllJobsAsync();
+        Task<Guid> CreateAsync(JobAdvertModel model);
+        Task DeactivateAsync(Guid jobId);
+        Task ActivateAsync(Guid jobId);
+        Task EditAsync(Guid jobId, JobAdvertModel model);
+        Task<bool> ExistsByIdAsync(Guid jobId);
+        Task<JobAdvertModel> GetByIdAsync(Guid jobId);
     }
 }
--- TheRealDealGym.Core/Services/JobService.cs
+++ TheRealDealGym.Core/Services/JobService.cs
@@ -34,5 +34,91 @@ namespace TheRealDealGym.Core.Services
                 .ThenBy(j => j.Title)
                 .ToListAsync();
         }
+
+        /// <summary>
+        /// This method creates a new job advert.
+        /// </summary>
+        public async Task<Guid> CreateAsync(JobAdvertModel model)
+        {
+            JobAdvert jobAdvert = new JobAdvert()
+            {
+                Title = model.Title,
+                Description = model.Description
+            };
+
+            await repository.AddAsync(jobAdvert);
+            await repository.SaveChangesAsync();
+
+            return jobAdvert.Id;
+        }
+
+        /// <summary>
+        /// This method activates a job advert.
+        /// </summary>
+        public async Task ActivateAsync(Guid jobAdvertId)
+        {
+            var jobAdvert = await repository.GetByIdAsync<JobAdvert>(jobAdvertId);
+
+            if (jobAdvert != null)
+            {
+                jobAdvert.IsActive = true;
+                await repository.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// This method deactivates a job advert.
+        /// </summary>
+        public async Task DeactivateAsync(Guid jobAdvertId)
+        {
+            var jobAdvert = await repository.GetByIdAsync<JobAdvert>(jobAdvertId);
+
+            if (jobAdvert != null)
+            {
+                jobAdvert.IsActive = false;
+                await repository.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// This method edits a selected job advert.
+        /// </summary>
+        public async Task EditAsync(Guid jobAdvertId, JobAdvertModel model)
+        {
+            var jobAdvert = await repository.GetByIdAsync<JobAdvert>(jobAdvertId);
+
+            if (jobAdvert != null)
+            {
+                jobAdvert.Title = model.Title;
+                jobAdvert.Description = model.Description;
+                await repository.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// This method checks if a job advert with a given Id exists.
+        /// </summary>
+        public async Task<bool> ExistsByIdAsync(Guid jobAdvertId)
+        {
+            return await repository.AllReadOnly<JobAdvert>()
+                 .AnyAsync(j => j.Id == jobAdvertId);
+        }
+
+        /// <summary>
+        /// This method finds and returns a job advert by a given Id
+        /// </summary>
+        public async Task<JobAdvertModel> GetByIdAsync(Guid jobAdvertId)
+        {
+            return await repository.AllReadOnly<JobAdvert>()
+                .Where(j => j.Id == jobAdvertId)
+                .Select(j => new JobAdvertModel()
+                {
+                    Id = j.Id,
+                    Title = j.Title,
+                    Description = j.Description,
+                    IsActive = j.IsActive
+                })
+                .FirstAsync();
+        }
     }
 }
