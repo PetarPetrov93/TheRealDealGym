b7ea6d091e08fee7d54f7c588d07fb0c05f07aa3
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-11-14 18:36:46 +0200

    edit class async returns invalid date and time (date)

--- TheRealDealGym.UnitTests/ClassServiceTests.cs
+++ TheRealDealGym.UnitTests/ClassServiceTests.cs
@@ -400,6 +400,30 @@ namespace TheRealDealGym.UnitTests
             }
         }
 
+        [Test]
+        public async Task EditAsync_ShouldNotBeEdited_InvalidDate()
+        {
+            var classToEdit = new ClassFormModel()
+            {
+                Title = "Edited Advanced Muay Thai Class",
+                Description = "This class is dited",
+                Date = DateTime.Now.AddDays(-5).ToString("yyyy-MM-dd"),
+                Time = DateTime.Now.ToString("HH:mm:ss"),
+                Price = 12m,
+                RoomId = Guid.Parse("b62f8c2e-f842-4812-ae27-70be5e24d309"),
+                SportId = Guid.Parse("91458b63-8fc3-479b-b3b8-a7a920ec984e")
+            };
+
+            try
+            {
+                await classService.EditAsync(Guid.Parse("ad61a644-76c7-4366-9686-82b65a42fd14"), classToEdit);
+            }
+            catch (Exception ex)
+            {
+                Assert.That(ex.Message, Is.EqualTo("Please set a valid date and time for this class."));
+            }
+        }
+
         [TearDown]
         public async Task TearDown()
         {
